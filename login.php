ef0a6bd16058a1feedb33a6ec5bd857cbd43264b
Ankita-saha2000 <59726959+Ankita-saha2000@users.noreply.github.com>
2020-04-18 18:09:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -17,7 +17,6 @@
                 <input type ="text" placeholder="User Name" class="txt" name="UserName">
                 <input type ="email" placeholder="Email" class="txt" name="Email">
                 <input type ="password" placeholder="Password" class="txt" name="Password">
-                <input type ="password" placeholder="Confirm Password" class="txt" name="Confirm Password">
                 <form action="index.php">
                 <input type="submit" value="Sign In" class="btn1">
                 </form>
