f9873705d8e65a65235e91697c71ec6643917862
Anubhav Kharwar <61133543+Anuk-1511@users.noreply.github.com>
2020-05-10 11:43:25 +0530

    Add files via upload

--- signup.php
+++ signup.php
@@ -63,9 +63,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+        $email = mysqli_real_escape_string($link, $_REQUEST['email']);
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password ,email) VALUES (?, ?,'$email')";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
